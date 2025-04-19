0940906e1aa4f2a647972cca30de436f7dc92a1c
dashuaige@berkeley.edu <xupeiran2048@gmail.com>
2025-04-19 15:13:38 -0500

    update

--- homeworks/hw1-main/badger-mart/jobs.js
+++ homeworks/hw1-main/badger-mart/jobs.js
@@ -2,4 +2,18 @@ function submitApplication(e) {
     e.preventDefault(); // You can ignore this; prevents the default form submission!
 
     // TODO: Alert the user of the job that they applied for!
+    const jobs = document.getElementsByName("job");
+    let selectedJob = null;
+    for (let i = 0; i < jobs.length; i++) {
+        if (jobs[i].checked) {
+            selectedJob = jobs[i].value;
+            break;
+        }
+    }
+    if (selectedJob) {
+        alert(`Thank you for applying to be a ${selectedJob}! `)
+    } else {
+        alert("Please select a job! ")
+    }
+
 }
--- homeworks/hw1-main/badger-mart/order.js
+++ homeworks/hw1-main/badger-mart/order.js
@@ -1,57 +1,57 @@
 const ITEMS = ["apple", "bagel", "coconut", "donut", "egg"]
 
 const SALES_TAX = {
-	AL:	0.040,
-	AK:	0.000,
-	AZ:	0.056,
-	AR:	0.065,
-	CA:	0.073,
-	CO:	0.029,
-	CT:	0.064,
-	DE:	0.000,
-	DC:	0.060,
-	FL:	0.060,
-	GA:	0.040,
-	HI:	0.040,
-	ID:	0.060,
-	IL:	0.063,
-	IN:	0.070,
-	IA:	0.060,
-	KS:	0.065,
-	KY:	0.060,
-	LA:	0.045,
-	ME:	0.055,
-	MD:	0.060,
-	MA:	0.063,
-	MI:	0.060,
-	MN:	0.069,
-	MS:	0.070,
-	MO:	0.042,
-	MT:	0.000,
-	NE:	0.055,
-	NV:	0.069,
-	NH:	0.000,
-	NJ:	0.066,
-	NM:	0.051,
-	NY:	0.040,
-	NC:	0.048,
-	ND:	0.050,
-	OH:	0.058,
-	OK:	0.045,
-	OR:	0.000,
-	PA:	0.060,
-	RI:	0.070,
-	SC:	0.060,
-	SD:	0.045,
-	TN:	0.070,
-	TX:	0.063,
-	UT:	0.061,
-	VT:	0.060,
-	VA:	0.053,
-	WA:	0.065,
-	WV:	0.060,
-	WI:	0.050,
-	WY:	0.040,
+	AL: 0.040,
+	AK: 0.000,
+	AZ: 0.056,
+	AR: 0.065,
+	CA: 0.073,
+	CO: 0.029,
+	CT: 0.064,
+	DE: 0.000,
+	DC: 0.060,
+	FL: 0.060,
+	GA: 0.040,
+	HI: 0.040,
+	ID: 0.060,
+	IL: 0.063,
+	IN: 0.070,
+	IA: 0.060,
+	KS: 0.065,
+	KY: 0.060,
+	LA: 0.045,
+	ME: 0.055,
+	MD: 0.060,
+	MA: 0.063,
+	MI: 0.060,
+	MN: 0.069,
+	MS: 0.070,
+	MO: 0.042,
+	MT: 0.000,
+	NE: 0.055,
+	NV: 0.069,
+	NH: 0.000,
+	NJ: 0.066,
+	NM: 0.051,
+	NY: 0.040,
+	NC: 0.048,
+	ND: 0.050,
+	OH: 0.058,
+	OK: 0.045,
+	OR: 0.000,
+	PA: 0.060,
+	RI: 0.070,
+	SC: 0.060,
+	SD: 0.045,
+	TN: 0.070,
+	TX: 0.063,
+	UT: 0.061,
+	VT: 0.060,
+	VA: 0.053,
+	WA: 0.065,
+	WV: 0.060,
+	WI: 0.050,
+	WY: 0.040,
 }
 
 function roundMoney(num) {
@@ -59,11 +59,23 @@ function roundMoney(num) {
 }
 
 function calculateSubtotal() {
-	return 0.00; // TODO calculateSubtotal
+	let res = 0;
+	const rows = document.getElementById("price-board").getElementsByTagName("tr");
+	console.log(rows)
+	for (let i = 1; i < rows.length; i++) {
+		let item = rows[i].getElementsByTagName("td")[0].innerText.toLowerCase().slice(0, -1);
+		console.log(item);
+		let price = parseFloat(document.getElementById(`${item}-price`).innerText);
+		let quantity = parseInt(document.getElementById(`${item}-quantity`).value);
+		res += quantity * price;
+	}
+	return res; // TODO calculateSubtotal
 }
 
 function calculateSalesTax() {
-	return 0.00; // TODO calculateSalesTax
+	const state = document.getElementById("state-tax").value;
+	console.log(state)
+	return getSalesTaxRateForState(state) * calculateSubtotal(); // TODO calculateSalesTax
 }
 
 function getSalesTaxRateForState(state) {
@@ -84,3 +96,6 @@ document.getElementById("btn-sales-tax").addEventListener("click", () => {
 });
 
 // TODO Add an event listener to btn-checkout
+document.getElementById("btn-checkout").addEventListener("click", () => {
+	alert("Your total is: $" + (calculateSalesTax() + calculateSubtotal()).toFixed(2));
+})

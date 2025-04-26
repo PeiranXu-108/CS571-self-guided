1c94e2d4f5fff98e271500e08f97f52dced064e7
dashuaige@berkeley.edu <xupeiran2048@gmail.com>
2025-04-25 21:41:14 -0500

    update

--- lectures/ice-webdev1-main/solution/src/components/Recipe.jsx
+++ lectures/ice-webdev1-main/solution/src/components/Recipe.jsx
@@ -1,45 +1,23 @@
 import { useState } from "react";
-import { Button, Table, Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
-export default function FeaturedItem(props) {
+export default function Recipe(props) {
 
-    const [showNutrition, setShowNutrition] = useState(false);
+    const [likes, setLikes] = useState(0);
 
-    function handleClick() {
-        setShowNutrition(oShow => !oShow);
+    function handleLike() {
+        setLikes(oLikes => oLikes + 1)
     }
 
-    return (
-      <Card style={{ margin: "auto", marginTop: "1rem", maxWidth: "40rem" }}>
-        <Card.Body>
-          <Card.Img variant="top" src={props.img} alt={props.description} />
-          <Card.Title style={{ marginTop: "1rem", fontWeight: "bold" }}>{props.name}</Card.Title>
-          <Card.Text><strong>Price:</strong> {props.price} per unit</Card.Text>
-          <Card.Text>{props.description}</Card.Text>
-          <Button variant="primary" onClick={handleClick} style={{ marginBottom: "1rem" }}>
-            {showNutrition ? "Hide Nutrition Facts" : "Show Nutrition Facts"}
-          </Button>
-          {showNutrition && (
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Calories</th>
-                  <th>Fat</th>
-                  <th>Carbohydrates</th>
-                  <th>Protein</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{props.nutrition?.calories ?? "0"}</td>
-                  <td>{props.nutrition?.fat ?? "0g"}</td>
-                  <td>{props.nutrition?.carbohydrates ?? "0g"}</td>
-                  <td>{props.nutrition?.protein ?? "0g"}</td>
-                </tr>
-              </tbody>
-            </Table>
-          )}
-        </Card.Body>
-      </Card>
-    );
+    return <Card style={{margin: "auto", marginTop: "1rem", maxWidth: "40rem"}}>
+        {
+            Object.keys(props).length > 0 ? <>
+                <img src={props.img.location} alt={props.img.description}/>
+                <h2>{props.name}</h2>
+                <p>by {props.author} | <strong>{likes} likes</strong></p>
+                <p>described as {props.keywords.join(", ")}</p>
+                <Button onClick={handleLike}>Like this Recipe</Button>
+            </> : <p>Loading...</p>
+        }
+    </Card>
 }
